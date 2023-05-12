2f90564e53e76cd400b098ee71cab51e0003d29f
bigsegs <cw.plastering.services@gmail.com>
2023-05-12 14:19:14 +0100

    this time

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.get('/api/articles/:article_id',getArticleById);
 
 app.get('/api/articles',getAllArticles);
 
+app.post('/api/articles/:article_id/comments',postComment)
+
 
 
 module.exports = app;
